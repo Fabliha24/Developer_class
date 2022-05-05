c469f8d913b903c0ced8be7c72b135eecbfb992d
Fabliha24 <93105300+Fabliha24@users.noreply.github.com>
2022-05-04 23:17:17 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,22 @@
+print("Hello World")
+sum = 1+2+3+\
+      4+5+6+7
+print(sum)
+sum1 = (1+2+3+4+
+        5+6+7)
+print(sum1)
+my_integer_variable = 1000
+my_float_variable = 3.5
+my_complex_variable = 1+2j
+print(my_integer_variable)
+print(my_float_variable)
+print(my_complex_variable)
+
+string = "This is a string variabale"
+all_words = string.split()
+first_word= all_words[2]
+print(first_word)
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print(a + b)
